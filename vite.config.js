d3fb1b8071fae36d40afcf7cedef11a607602261
teddyj <teddyj@gmail.com>
2025-04-13 16:56:35 -0400

    Update configuration for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ad-manager/', // Update this to match your repository name
+  base: '/ad-manager/', // Matches the repository name exactly
 })
